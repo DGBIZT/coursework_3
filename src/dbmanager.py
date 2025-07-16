a32a8069f377976b52f107a5aa0ec3008eb66135
DGBIZT <georgiy.gorlov@gmail.com>
2025-07-16 20:52:04 +0300

    Создал class DBManager, подключение к БД, контекстный менеджер для работы с курсором и закрытие соединения с БД

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -1,5 +1,67 @@
+import psycopg2
+from dotenv import load_dotenv
+from psycopg2 import sql, extras
+import os
+from contextlib import contextmanager
+
 
 class DBManager:
+    def __init__(self):
+        """
+        # Загружаем переменные окружения из .env файла
+        """
+        load_dotenv()
+
+        self.dbname = os.getenv("DB_NAME")
+        self.user = os.getenv("DB_USER")
+        self.password = os.getenv("DB_PASSWORD")
+        self.host = os.getenv("DB_HOST", 'localhost')
+        self.port = os.getenv("DB_PORT", '5432')
+        self.connection = None
+
+    def connect(self):
+        """
+        Метод для установления соединения с БД
+        """
+        try:
+            self.connection = psycopg2.connect(
+                dbname=self.dbname,
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port
+            )
+            print("Подключение к БД установлено")
+        except psycopg2.Error as e:
+            print(f"Ошибка при подключении к БД: {e}")
+
+    @contextmanager
+    def get_cursor(self):
+        """
+        Контекстный менеджер для работы с курсором
+        """
+        try:
+            with self.connection.cursor() as cursor:
+                yield cursor
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")
+            self.connection.rollback() # это метод, который выполняет откат (отмену) всех изменений, сделанных в рамках текущей транзакции базы данных.
+            # т.е., последовательность операций, которые либо выполняются полностью, либо не выполняются вообще.
+
+            raise
+        finally:
+            self.connection.commit()
+
+
+
+
+    def close(self):
+        """
+        Закрытие соединения с БД
+        """
+        if self.connection:
+            self.connection.close()
+            print("Соединение с БД Закрыто")
 
         # метод подключения к БД (cur, conn)
 
